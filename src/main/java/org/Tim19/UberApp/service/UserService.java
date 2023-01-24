dbe3a5a6b9c432eed15ce8ecc3527cf6b312f144
Tamara Dzambic <dzambic.sv43.2020@uns.ac.rs>
2023-01-25 00:38:24 +0100

    Small notes fix

--- src/main/java/org/Tim19/UberApp/service/UserService.java
+++ src/main/java/org/Tim19/UberApp/service/UserService.java
@@ -1,7 +1,6 @@
 package org.Tim19.UberApp.service;
 
 import org.Tim19.UberApp.dto.PaginatedData.RidePaginatedDTO;
-import org.Tim19.UberApp.dto.PaginatedData.UserPanicPaginatedDTO;
 import org.Tim19.UberApp.model.ResetCode;
 import org.Tim19.UberApp.model.User;
 import org.Tim19.UberApp.repository.ResetCodeRepository;
@@ -14,7 +13,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -22,9 +20,6 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private RideRepository rideRepository;
-
     @Autowired
     private ResetCodeRepository resetCodeRepository;
 
@@ -69,23 +64,8 @@ public class UserService {
     public void remove(Integer id){userRepository.deleteById(id);}
 
     public Set<RidePaginatedDTO> findAllRides(Integer id){
-        //Set<String> result = userRepository.findAllRides(id);
 
         Set<RidePaginatedDTO> rides = new HashSet<>();
-
-//        for (String s: result) {
-//            String[] res = s.split(",");
-//            RidePaginatedDTO ride = new RidePaginatedDTO();
-//            //mozda neki mapper
-//
-//            ride.setId(Integer.valueOf(res[0]));
-//            ride.setBabyTransport(Boolean.valueOf(res[1]));
-//            //ride.setEndTime(LocalDateTime.parse(res[2]));
-//            ride.setEstimatedTimeInMinutes(Integer.valueOf(res[3]));
-//            ride.setPetTransport(Boolean.valueOf(res[5]));
-//
-//            rides.add(ride);
-//        }
 
         return rides;
     }
