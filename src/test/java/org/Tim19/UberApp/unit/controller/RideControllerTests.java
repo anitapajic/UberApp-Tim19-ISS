d8d4078bb5f92873415b6eff16eb8bb3ad89b158
Tamara Dzambic <dzambic.sv43.2020@uns.ac.rs>
2023-02-05 23:15:18 +0100

    Created test packages

---
+++ src/test/java/org/Tim19/UberApp/unit/controller/RideControllerTests.java
@@ -0,0 +1,7 @@
+package org.Tim19.UberApp.unit.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RideControllerTests {
+}
---
+++ src/test/java/org/Tim19/UberApp/unit/service/RideServiceTests.java
@@ -0,0 +1,7 @@
+package org.Tim19.UberApp.unit.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RideServiceTests {
+}
